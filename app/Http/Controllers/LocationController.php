d929520a733ca37df50ca9c83b63784f671d53ad
unknown <melanyrm2507@gmail.com>
2024-11-04 17:35:36 -0400

    Creado rutas de Locations

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Location;
 
 use Illuminate\Http\Request;
 
 class LocationController extends Controller
 {
-    //
+    //metodo index retornara todas las localizaciones 
+    public function index(){
+        return Location::all();
+    }
 }
--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Location extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = [
+        'name',
+        'address',
+        'city',
+        'latitude',
+        'longitude',
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
